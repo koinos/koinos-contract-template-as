ecb6da41c9e3fa20b4ae84689978e4d143473a53
Nathaniel Caldwell <nbcwell@gmail.com>
2022-08-19 13:20:03 -0400

    Initial commit

---
+++ assembly/Template.ts
@@ -0,0 +1,9 @@
+import { chain, System, Base64, Base58, Token, Crypto, template, Arrays, StringBytes } from "koinos-sdk-as";
+
+export class Template {
+  example(args: template.example_arguments): template.example_result {
+    
+
+    return new template.example_result();
+  }
+}
