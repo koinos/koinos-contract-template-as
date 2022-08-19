83650e0d682bb36f9d40325cb93cf6fe018069e7
Michael Vandeberg <mvandeberg@users.noreply.github.com>
2022-08-19 11:37:28 -0700

    Progress on fixing proto generation

--- assembly/Template.ts
+++ assembly/Template.ts
@@ -1,9 +1,15 @@
-import { chain, System, Base64, Base58, Token, Crypto, template, Arrays, StringBytes } from "koinos-sdk-as";
+import { chain, System, Base64, Base58, Token, Crypto, Arrays, StringBytes } from "@koinos/sdk-as";
+import { template } from "./proto/template";
 
 export class Template {
   example(args: template.example_arguments): template.example_result {
-    
+    // const value = args.value;
 
-    return new template.example_result();
+    // YOUR CODE HERE
+
+    const res = new template.example_result();
+    // res.value = ;
+
+    return res;
   }
 }
